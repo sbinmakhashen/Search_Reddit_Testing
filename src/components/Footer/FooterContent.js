71dacb7d29e91ebbe00260b9007c7c9e3de93b70
Salem <nlsabm@yahoo.com>
2021-09-10 15:59:26 -0400

    changes to footer component

---
+++ src/components/Footer/FooterContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import FooterLogo from './FooterLogo';
+
+const FooterContent = () => {
+  return (
+    <footer>
+      <p>subreddit-posts-finder.io</p>
+      <FooterContent />
+      <p>Terms & Privacy</p>
+    </footer>
+  );
+};
+
+export default FooterContent;
---
+++ src/components/Footer/FooterLogo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import footerLogo from '../../img/footer.svg';
+const FooterLogo = () => {
+  return (
+    <>
+      <img src={footerLogo} alt="Footer logo" className="logo" />
+    </>
+  );
+};
+
+export default FooterLogo;
