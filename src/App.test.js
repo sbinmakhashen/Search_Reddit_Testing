bafa92e12277b756e0d771dbaf53b19f60ba6464
Salem <nlsabm@yahoo.com>
2021-09-12 22:33:46 -0400

    Header unit testing setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('Header', () => {
+  describe('"How it works" link points to the correct page', () => {
+    render(
+      <MemoryRouter>
+        <App />
+      </MemoryRouter>,
+    );
+  });
 });
